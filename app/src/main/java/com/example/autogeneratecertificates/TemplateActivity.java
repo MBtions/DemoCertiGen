e7bfd0143cefd2433039839de721edbe21ba31ce
Khushi Srivastava <khushi.srivastava145@gmail.com>
2021-07-21 14:33:51 +0530

    creating certificates in ppt

--- app/src/main/java/com/example/autogeneratecertificates/TemplateActivity.java
+++ app/src/main/java/com/example/autogeneratecertificates/TemplateActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,13 +14,16 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFShape;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.apache.poi.xslf.usermodel.XSLFSlideLayout;
 import org.apache.poi.xslf.usermodel.XSLFTextBox;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,6 +35,12 @@ public class TemplateActivity extends AppCompatActivity {
     String[] c=new String[30];
     String[] d=new String[30];
     String[] e=new String[30];
+    String[] name=new String[30];
+    String[] course=new String[30];
+    String[] college=new String[30];
+    String[] position=new String[30];
+    String[] society=new String[30];
+
     double row_num=0.0;
     String rows="0.0";
 
@@ -61,19 +71,25 @@ public class TemplateActivity extends AppCompatActivity {
                 XSSFSheet sheet = workbook.getSheetAt(0);
                 Iterator<Row> rowIterator = sheet.iterator();
                 int count = 0;
+                String temp;
                 while (rowIterator.hasNext() && count < row_num+1) {
                     Row row = rowIterator.next();
-                    Iterator<Cell> c = row.cellIterator();
-                    test.post(() -> test.setText(c.next().getStringCellValue()));
-                    Thread.sleep(1000);
-                    test.post(() -> test.setText(c.next().getStringCellValue()));
-                    Thread.sleep(1000);
-                    test.post(() -> test.setText(c.next().getStringCellValue()));
-                    Thread.sleep(1000);
-                    test.post(() -> test.setText(c.next().getStringCellValue()));
-                    Thread.sleep(1000);
-                    test.post(() -> test.setText(c.next().getStringCellValue()));
-                    Thread.sleep(1000);
+                    Iterator<Cell> cx = row.cellIterator();
+                    temp=cx.next().getStringCellValue();
+                    a[count]=temp;
+
+                    temp=cx.next().getStringCellValue();
+                    b[count]=temp;
+
+                    temp=cx.next().getStringCellValue();
+                    c[count]=temp;
+
+                    temp=cx.next().getStringCellValue();
+                    d[count]=temp;
+
+                    temp=cx.next().getStringCellValue();
+                    e[count]=temp;
+
                     count++;
                 }
                 inputfile.close();
@@ -83,13 +99,149 @@ public class TemplateActivity extends AppCompatActivity {
             } catch (IOException e) {
                 test.setText("IOException");
                 e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
         }).start();
 
     }
 
+    public void matchCoulmn() {
+        int i;
+        switch (a[0].toLowerCase()) {
+            case "name":
+                for (i = 1; i <= row_num; i++) {
+                    name[i-1] = a[i];
+                }
+                break;
+            case "college":
+                for (i=1; i<=row_num; i++){
+                    college[i-1]=a[i];
+                }
+                break;
+            case "course":
+                for (i=1; i<=row_num; i++){
+                    course[i-1]=a[i];
+                }
+                break;
+            case "society":
+                for (i=1; i<=row_num; i++){
+                    society[i-1]=a[i];
+                }
+                break;
+            case "position":
+                for (i=1; i<=row_num; i++){
+                    position[i-1]=a[i];
+                }
+                break;
+        }
+        switch (b[0].toLowerCase()) {
+            case "name":
+                for (i = 1; i <= row_num; i++) {
+                    name[i-1] = b[i];
+                }
+                break;
+            case "college":
+                for (i=1; i<=row_num; i++){
+                    college[i-1]=b[i];
+                }
+                break;
+            case "course":
+                for (i=1; i<=row_num; i++){
+                    course[i-1]=b[i];
+                }
+                break;
+            case "society":
+                for (i=1; i<=row_num; i++){
+                    society[i-1]=b[i];
+                }
+                break;
+            case "position":
+                for (i=1; i<=row_num; i++){
+                    position[i-1]=b[i];
+                }
+                break;
+        }
+        switch (c[0].toLowerCase()) {
+            case "name":
+                for (i = 1; i <= row_num; i++) {
+                    name[i-1] = c[i];
+                }
+                break;
+            case "college":
+                for (i=1; i<=row_num; i++){
+                    college[i-1]=c[i];
+                }
+                break;
+            case "course":
+                for (i=1; i<=row_num; i++){
+                    course[i-1]=c[i];
+                }
+                break;
+            case "society":
+                for (i=1; i<=row_num; i++){
+                    society[i-1]=c[i];
+                }
+                break;
+            case "position":
+                for (i=1; i<=row_num; i++){
+                    position[i-1]=c[i];
+                }
+                break;
+        }
+        switch (d[0].toLowerCase()) {
+            case "name":
+                for (i = 1; i <= row_num; i++) {
+                    name[i-1] = d[i];
+                }
+                break;
+            case "college":
+                for (i=1; i<=row_num; i++){
+                    college[i-1]=d[i];
+                }
+                break;
+            case "course":
+                for (i=1; i<=row_num; i++){
+                    course[i-1]=d[i];
+                }
+                break;
+            case "society":
+                for (i=1; i<=row_num; i++){
+                    society[i-1]=d[i];
+                }
+                break;
+            case "position":
+                for (i=1; i<=row_num; i++){
+                    position[i-1]=d[i];
+                }
+                break;
+        }
+        switch (e[0].toLowerCase()) {
+            case "name":
+                for (i = 1; i <= row_num; i++) {
+                    name[i-1] = e[i];
+                }
+                break;
+            case "college":
+                for (i=1; i<=row_num; i++){
+                    college[i-1]=e[i];
+                }
+                break;
+            case "course":
+                for (i=1; i<=row_num; i++){
+                    course[i-1]=e[i];
+                }
+                break;
+            case "society":
+                for (i=1; i<=row_num; i++){
+                    society[i-1]=e[i];
+                }
+                break;
+            case "position":
+                for (i=1; i<=row_num; i++){
+                    position[i-1]=e[i];
+                }
+                break;
+        }
+    }
     private void searchSlideAsync() {
         new Thread(this::searchSlide).start();
     }
@@ -98,18 +250,50 @@ public class TemplateActivity extends AppCompatActivity {
         try {
             AssetManager assManager = getAssets();
             InputStream is = assManager.open("templates.pptx");
+            OutputStream os = new FileOutputStream("output.pptx");
             XMLSlideShow ppt = new XMLSlideShow(is);
-            XSLFSlide slide = ppt.getSlides().get(0);
-            List<XSLFShape> shapes = slide.getShapes();
-            for (XSLFShape shape : shapes) {
-                if (shape instanceof XSLFTextBox) {
-                    String text = ((XSLFTextBox) shape).getText();
-                    String newText = text.replace("<Name>", "Saurabh");
-                    ((XSLFTextBox) shape).setText(newText);
+            XSLFSlide mainslide = ppt.getSlides().get(0);
+
+            for (int i=0; i<row_num; i++){
+                XSLFSlideLayout layout = mainslide.getSlideLayout();
+                XSLFSlide newSlide = ppt.createSlide(layout);
+                newSlide.importContent(mainslide);
+            }
+            int count=0;
+
+            List<XSLFSlide> slides=ppt.getSlides();
+            for (XSLFSlide slide : slides) {
+                List<XSLFShape> shapes = slide.getShapes();
+                for (XSLFShape shape : shapes) {
+                    if (shape instanceof XSLFTextBox) {
+                        String text = ((XSLFTextBox) shape).getText();
+                        text = text.replace("<Name>", name[count] );
+                        text = text.replace("<Course>", course[count] );
+                        text = text.replace("<College>", college[count] );
+                        text = text.replace("<Position>", position[count] );
+                        text = text.replace("<Society>", society[count] );
+                        ((XSLFTextBox) shape).setText(text);
+                        ppt.write(os);
+                    }
                 }
             }
-            Log.d("TemplateActivity", "shapes " + shapes.size());
-        } catch (IOException e) {
+            Log.d("TemplateActivity", "shapes " + slides.size());
+            if(os!=null) {
+                try {
+                    os.close();
+                    os = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(is!=null){
+                try{
+                    is.close();
+                    is=null;
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+        } }catch (IOException e) {
             e.printStackTrace();
         }
     }
